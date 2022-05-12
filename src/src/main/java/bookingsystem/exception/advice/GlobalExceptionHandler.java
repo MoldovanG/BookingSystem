4533e47f34aa888aeddf318d91f7b9a6a04b42fb
firsthug <first000hug@gmail.com>
2022-05-12 23:16:59 +0300

    commit room add ons and pagination

--- src/src/main/java/bookingsystem/exception/advice/GlobalExceptionHandler.java
+++ src/src/main/java/bookingsystem/exception/advice/GlobalExceptionHandler.java
@@ -1,15 +1,19 @@
 package com.moldovan.uni.bookingsystem.exception.advice;
 
+import com.moldovan.uni.bookingsystem.exceptions.RoomRegistrationUnavailableException;
 import com.moldovan.uni.bookingsystem.service.RoomAllreadyBookedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
 import javax.persistence.EntityNotFoundException;
 import java.time.LocalTime;
+import java.util.NoSuchElementException;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(EntityNotFoundException.class)
+    @ExceptionHandler({EntityNotFoundException.class, NoSuchElementException.class})
     public ResponseEntity<String> handle(EntityNotFoundException e){
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(LocalTime.now() +  "- Entity was not found and failed with message :  " + e.getMessage());
@@ -19,6 +23,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handle(RoomAllreadyBookedException e){
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(LocalTime.now() +  "- Booking was not registered. One of the rooms is already booked for the selected period :  " + e.getMessage());
+    }
+
+    @ExceptionHandler(RoomRegistrationUnavailableException.class)
+    public ResponseEntity<String> handle(RoomRegistrationUnavailableException e){
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body(LocalTime.now() +  "- Room registration currently unavailable ");
     }
 
 }
--- src/src/main/java/bookingsystem/repository/BookingRepository.java
+++ src/src/main/java/bookingsystem/repository/BookingRepository.java
@@ -1,7 +1,7 @@
 package com.moldovan.uni.bookingsystem.repository;
 
 import com.moldovan.uni.bookingsystem.domain.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface BookingRepository extends JpaRepository<Booking, Long> {
+public interface BookingRepository extends PagingAndSortingRepository<Booking, Long> {
 }
