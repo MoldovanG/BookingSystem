9faa6441de249222b9d885a385286c2b958d7bb6
firsthug <first000hug@gmail.com>
2022-05-18 02:12:47 +0300

    add http security

--- bookingsystem/src/main/java/bookingsystem/auth/MssqlSecurityConfig.java
+++ bookingsystem/src/main/java/bookingsystem/auth/MssqlSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -40,9 +41,12 @@ public class MssqlSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/api/room/**").hasRole("CUSTOMER")
+                .antMatchers(HttpMethod.OPTIONS,"/api/**")
+                .hasRole("CUSTOMER")
                 .and()
-                .formLogin();
+                .httpBasic()
+                .and()
+                .csrf().disable();
 
     }
 }
